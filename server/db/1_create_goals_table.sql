ec4e08b1123704b17532339b2f8d69cafdc227b2
aklambert <alanaks558@gmail.com>
2025-03-14 08:46:31 -0500

    #20, Retry commmit to fix file structure

--- server/db/1_create_goals_table.sql
+++
@@ -1,6 +0,0 @@
--- SQLite
-CREATE TABLE IF NOT EXISTS goals(
-  goal_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  goal_descrip TEXT NOT NULL,
-  goal_create_date TEXT NOT NULL DEFAULT current_timestamp
-);
