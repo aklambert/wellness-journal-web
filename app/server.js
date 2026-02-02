2799e9579eead3387c0020f509ac7cc9b10488be
aklambert <alanaks558@gmail.com>
2026-02-02 17:07:09 -0600

    #21, Define API endpoint routes for login and signup

--- app/server.js
+++ app/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const { login, signup } = require('./controllers/authroutes');
 
 const app = express();
 
@@ -11,12 +10,19 @@ const corsOptions = {
   //credentials: true,
 }
 
+app.use(express.json());
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+// POST /api/login - Authenticate user and return JWT
+app.post('/api/login', login);
+
+// POST /api/signup - Create new user account
+app.post('/api/signup', signup);
+
+app.listen(process.env.NODEPORT, () => {
+  console.log(`Example app listening on port ${process.env.NODEPORT}`)
 });
